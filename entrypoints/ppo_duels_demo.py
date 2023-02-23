e23c0422bbb2b464856103b71375b2da1fb987d7
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-02-23 19:02:40 +0000

    Add ppo demo

---
+++ entrypoints/ppo_duels_demo.py
@@ -0,0 +1,14 @@
+from stable_baselines3 import PPO
+from pettingzoo.test import api_test
+
+from common import paths
+from environment.rules import Rules
+from environment.envs import duels_v0
+
+
+rules = Rules(paths.BIN_DIR/"rules.dll")
+env = duels_v0.env(rules)  # Create a default duels environment
+api_test(env)
+
+model = PPO('MlpPolicy', env, verbose=1)
+model.learn(total_timesteps=10000)
--- environment/rules.py
+++ environment/rules.py
@@ -11,7 +11,15 @@ class Rules:
         self,
         dll_path: pathlib.Path,
     ) -> None:
-        dll = ctypes.CDLL(dll_path)
+        self.dll_path = dll_path
+        self._setup = None
+        self._reset = None
+        self._step = None
+        self._done = None
+        self._render = None
+    
+    def load(self):
+        dll = ctypes.CDLL(self.dll_path)
 
         setup = dll.setup
         setup.argtypes = [ctypes.c_char_p]
@@ -35,26 +43,43 @@ class Rules:
         self._step = step
         self._done = done
         self._render = render
-    
+
+    def unload(self):
+        self._setup = None
+        self._reset = None
+        self._step = None
+        self._done = None
+        self._render = None
+
+    def isloaded(self):
+        loaded_attrs = [self._setup, self._reset, self._step, self._done, self._render]
+        assert all(x is None for x in loaded_attrs) or all(x is not None for x in loaded_attrs), "the dll is in an inconsistent state"
+        return self._setup is not None
+
     def moves(self) -> list[str]:
         return self._MOVES
 
     def render(self, color: bool = True) -> None:
+        assert self.isloaded(), "the dll has not been loaded"
         self._render(1 if color else 0)
 
     def done(self) -> bool:
+        assert self.isloaded(), "the dll has not been loaded"
         return self._done() == 1
 
     def setup(self, options: dict) -> None:
+        assert self.isloaded(), "the dll has not been loaded"
         options = json.dumps(options).encode("utf-8")
         self._setup(options)
 
     def reset(self, options: dict) -> dict:
+        assert self.isloaded(), "the dll has not been loaded"
         options = json.dumps(options).encode("utf-8")
         res = self._reset(options)
         return json.loads(res.decode("utf-8"))
 
     def step(self, actions: dict) -> dict:
+        assert self.isloaded(), "the dll has not been loaded"
         actions = json.dumps(actions).encode("utf-8")
         res = self._step(actions)
         return json.loads(res.decode("utf-8"))
