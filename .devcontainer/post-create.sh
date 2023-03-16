97ed90cab44a1d50d90e114ea03bde8027133d3e
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-03-16 00:16:31 +0000

    Use poetry for packaging

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-pip install -r dev-requirements.txt
-pip install -r requirements.txt
+curl -sSL https://install.python-poetry.org | python3 -
+poetry config virtualenvs.in-project true
+poetry install
+# Depends on Gym 0.21 which is not supported for installation by poetry.
+# Must use a workaround (found below) until updated to
+# use Gymnasium (see: https://github.com/DLR-RM/stable-baselines3/pull/1327).
+# TODO: install using poetry once stable-baselines3 is updated to use Gymnasium
+# stable-baselines3 = "==1.7.0"
+poetry run pip install stable-baselines3==1.7.0
