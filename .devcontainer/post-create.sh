ba26e03b86ccd6aa5587f09433312620f6296ff8
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-02-24 23:22:56 +0000

    chore: specify feature versions in devcontainer config

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,6 +3,7 @@
 curl -sSL https://install.python-poetry.org | python3 -
 poetry config virtualenvs.in-project true
 poetry install
+
 # Depends on Gym 0.21 which is not supported for installation by poetry.
 # Must use a workaround (found below) until updated to
 # use Gymnasium (see: https://github.com/DLR-RM/stable-baselines3/pull/1327).
